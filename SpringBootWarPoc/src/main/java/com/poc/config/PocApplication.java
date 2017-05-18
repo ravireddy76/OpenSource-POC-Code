728fcbf235373bacff5d175762aed8a9bfc17695
Ravi.Reddy2 <Ravi.Reddy2@target.com>
2017-05-17 22:41:10 -0500

    Updated code changes to make Spring boot as war file.

--- SpringBootWarPoc/src/main/java/com/poc/config/PocApplication.java
+++ SpringBootWarPoc/src/main/java/com/poc/config/PocApplication.java
@@ -2,25 +2,46 @@ package com.poc.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan(basePackages = {"com.poc"})
-public class PocApplication {
+@ComponentScan(basePackages = { "com.poc" })
+@Import({ SwaggerConfig.class })
+public class PocApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-		SpringApplication.run(PocApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication sa = new SpringApplication(PocApplication.class);
+		sa.run(args);
 	}
-    
-}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PocApplication.class);
+	}
+
+//	@Bean
+//	public String applicationName() {
+//		return "springboot-war-poc";
+//	}
+}
 
 //public class PocApplication extends SpringBootServletInitializer {
 //@Override
 //protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //  return application.sources(PocApplication.class);
 //}
+//}
+
+//public class PocApplication {
+//
+//    public static void main(String[] args) {
+//		SpringApplication.run(PocApplication.class, args);
+//	}
+//    
 //}
--- SpringBootWarPoc/src/main/java/com/poc/controllers/UserController.java
+++ SpringBootWarPoc/src/main/java/com/poc/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.poc.domain.User;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
 	@RequestMapping(value = "/existingusers", method = RequestMethod.GET, produces = "application/json")
